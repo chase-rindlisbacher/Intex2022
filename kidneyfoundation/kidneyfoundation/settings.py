0b1bef17c4245794bc364ba9ba6172ff58a2a8ca
chase.rindlisbacher <cjrjazz20@gmail.com>
2022-11-28 15:10:55 -0700

    databases settings.py

--- kidneyfoundation/kidneyfoundation/settings.py
+++ kidneyfoundation/kidneyfoundation/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'kidneys',
         'USER': 'postgres',
-        'PASSWORD': 'joRdaN23#1', 
+        'PASSWORD': '', 
         'HOST': 'localhost'
     }
 }
