59a77a8defcbe6bce0e2b649d341a161496a1d9d
Dalyn Baldwin <dlbaldwi@byu.edu>
2022-11-28 14:05:59 -0700

    patient added

--- kidneyfoundation/client_app/models.py
+++ kidneyfoundation/client_app/models.py
@@ -6,3 +6,17 @@ class Person(models.Model):
     last_name = models.CharField(max_length=30)
     phone = models.CharField(max_length=13)
     email = models.CharField(max_length=30)
+
+    @property
+    def __str__(self):
+        return (f'{self.last_name}, {self.first_name}')
+
+    class META:
+        db_table = 'person'
+
+class Patient(models.Model):
+    patient_id = models.ForeignKey(Person, on_delete=models.CASCADE)
+    age = models.FloatField()
+    height = models.IntegerField()
+    weight = models.IntegerField()
+    birthday = models.DateField()
