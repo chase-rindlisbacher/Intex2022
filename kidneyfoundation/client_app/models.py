a90710cd89c0e4b87d864d903edc13a7488b4dc9
Dalyn Baldwin <dlbaldwi@byu.edu>
2022-11-28 13:33:10 -0700

    created model for person

--- kidneyfoundation/client_app/models.py
+++ kidneyfoundation/client_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    phone = models.CharField(max_length=13)
+    email = models.CharField(max_length=30)
