e7cf1fc0884de49aea2341571032d7d87d05647c
Dalyn Baldwin <dlbaldwi@byu.edu>
2022-12-01 09:11:06 -0700

    fixed blue hue to show up better

--- kidneyfoundation/client_app/views.py
+++ kidneyfoundation/client_app/views.py
@@ -98,20 +98,6 @@ def newAccountPageView(request) :
     }
     return render(request, 'client_app/new_user.html', context)
 
-def myAccountPageView(request) :
-    if (request.method == 'POST'):
-        form = NewUserForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            messages.success(request, 'Registration successful.' )
-            return redirect('index')
-        messages.error(request, 'Unsuccessful registration. Invalid information.')
-    form = NewUserForm()
-    messages.success(request, 'Loaded')
-    return render (request, 'client_app/register.html', {'register_form': form})
-
-
 def myMenuView(request):
     return render(request, 'client_app/mymenu.html')
     
