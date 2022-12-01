437f154b36ee31ab698e351d7a9d348e6342b6a0
ty-327 <tyisb7@gmail.com>
2022-12-01 11:26:06 -0700

    take out crispy forms

--- kidneyfoundation/kidneyfoundation/settings.py
+++ kidneyfoundation/kidneyfoundation/settings.py
@@ -41,10 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'client_app.apps.ClientAppConfig',
-    'crispy_forms',
 ]
-
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
