45b9743023df0cf644b3fab1921cc872f47509fe
ty-327 <tyisb7@gmail.com>
2022-12-01 23:10:22 -0700

    views and settings

--- kidneyfoundation/kidneyfoundation/settings.py
+++ kidneyfoundation/kidneyfoundation/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'kidneys',
         'USER': 'postgres',
-        'PASSWORD': 'joRdaN23#1', 
+        'PASSWORD': 'is303', 
         'HOST': 'localhost'
     }
 }
